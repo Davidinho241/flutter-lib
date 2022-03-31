08d35f111892e18bbf81d027bff2680466a229e1
FlutLab.io <>
2022-03-31 17:29:59 +0000

    Autosave

--- lib/src/screens/HomeUI.dart
+++ lib/src/screens/HomeUI.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/buttons.dart';
+import 'MapUI.dart';
 
 class HomeUI extends StatefulWidget {
   const HomeUI({Key? key}) : super(key: key);
@@ -30,7 +31,16 @@ class _HomeUIState extends State<HomeUI> {
           padding: EdgeInsets.all(8.0),
           child: Column(
             children: [
-              MainButton(title: "Open the map", onTap: () async {}, icon: Icons.map_outlined, color: Colors.greenAccent),
+              MainButton(
+                  title: "Open the map",
+                  onTap: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MapUI()),
+                    );
+                  },
+                  icon: Icons.map_outlined,
+                  color: Colors.greenAccent),
               SizedBox(height: 20.0),
               MainButton(title: "Open the dialog", onTap: () async {}, icon: Icons.chrome_reader_mode_outlined, color: Colors.greenAccent),
             ],
